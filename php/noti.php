0a1e76a30d4452464f0c85b811694f3db1f73511
pepcs0530 <apiwich_cs_kmitl@hotmail.com>
2016-05-12 22:52:08 +0700

    แก้ระบบ noti

--- php/noti.php
+++ php/noti.php
@@ -4,18 +4,22 @@
     $data=array();
 
     //  คำร้องขอสารเคมี
-    $data[0]=0;
+    $sql = "SELECT COUNT(*) FROM `chem_receipt`
+            WHERE `cr_status`= '0' ";
+    $query = mysql_query($sql);
+    $row = mysql_fetch_array ($query);
+    $data[0]=$row[0];
 
     //  คำร้องขอสารเคมีเพิ่ม
     $data[1]=0;
 
     //  คำร้องขออื่นๆ
-    $sql = "SELECT COUNT(*) FROM `chem_request_other`
-        WHERE `cro_status`= '0' ;";
-    $query = mysql_query($sql);
+    $sql3 = "SELECT COUNT(*) FROM `chem_request_other`
+        WHERE `cro_status`= '0' ";
+    $query3 = mysql_query($sql3);
     
-    $row = mysql_fetch_array ($query);
-    $data[2]=$row[0];
+    $row3 = mysql_fetch_array ($query3);
+    $data[2]=$row3[0];
     
     
     echo json_encode($data, JSON_NUMERIC_CHECK);
