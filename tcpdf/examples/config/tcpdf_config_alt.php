eb77982c504c0a34edb5f979c3a434805c4a2cb9
pepcs0530 <apiwich_cs_kmitl@hotmail.com>
2016-05-24 16:42:25 +0700

    เปลี่ยน LOGO ขาวดำ

--- tcpdf/examples/config/tcpdf_config_alt.php
+++ tcpdf/examples/config/tcpdf_config_alt.php
@@ -70,7 +70,7 @@ define ('K_PATH_IMAGES', dirname(__FILE__).'/../images/');
  * Please set here your own logo or an empty string to disable it.
  */
 //define ('PDF_HEADER_LOGO', 'tcpdf_logo.jpg');
-define ('PDF_HEADER_LOGO', 'logo_kmitl.gif');
+define ('PDF_HEADER_LOGO', 'LogoBW.png');
 
 /**
  * Header logo image width in user units.
